501ac373208b296a7e97d28d261fc67eb6266375
Sam Kim <kimsj@rose-hulman.edu>
2012-10-22 14:32:37 -0400

    Ingredient list uses cursor

--- AndroidCocktails/src/edu/rosehulman/cocktailguide/IngredientListActivity.java
+++ AndroidCocktails/src/edu/rosehulman/cocktailguide/IngredientListActivity.java
@@ -1,20 +1,28 @@
 package edu.rosehulman.cocktailguide;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ListView;
+import edu.rosehulman.cocktailguide.db.DBHelper;
 
 public class IngredientListActivity extends Activity implements OnClickListener {
 
+	private Cursor mCursor;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient_list_view);
         
-        ((Button)findViewById(R.id.editButton1)).setOnClickListener(this);
+        mCursor = DBHelper.getInstance(this).getAllIngredients();
+        final IngredientItemAdapter adapter = new IngredientItemAdapter(this, mCursor);
+        
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 
     @Override
